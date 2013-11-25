60baac13af139d80a6235bcee6eb6cb85e295183
pinzolo <pinzolo@gmail.com>
2013-11-25 15:50:33 +0900

    Delete README.rdoc and modify description

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ require "bravo_hooks"
 Redmine::Plugin.register :redmine_bravo do
   name 'Redmine Bravo plugin'
   author 'pinzolo'
-  description 'This is a plugin of Redmine that provides function like "Facebook like"'
+  description 'This is a plugin of Redmine that provides feature like "Facebook like"'
   version '0.0.1'
   url 'https://github.com/pinzolo/redmine_bravo'
   author_url 'https://github.com/pinzolo'
